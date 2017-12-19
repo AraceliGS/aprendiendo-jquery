cbefd498ea7bf5e663c22413ae23333ff6f60c70
AraceliGS <aragutarras@gmail.com>
2017-12-19 18:23:29 -0500

    subiendo avances

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 /* Seleccionando por id */
 // console.log($('#p-test').text('Agregando contenido'));
 console.log($('#p-test').html('Hola'));
+//$('#p-test').html('Hola');
 console.log($('#input-test').val());
 });
 
@@ -19,3 +20,14 @@ console.log($('.p-text').html());
 console.log($('.input').val());
 */
 
+/*
+//$div = $('.father');
+console.log(div:first-child);
+console.log(div:last-child);
+console.log(div:nth-child);
+//div.first-child
+*/
+
+/* console.log($(div:first-child).text());
+console.log($(div:last-child).text());
+console.log($(div:nth-child(2)).text()); */

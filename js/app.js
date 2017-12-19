5b9e70948e049323da65e90717d7d8036abc27ee
AraceliGS <aragutarras@gmail.com>
2017-12-19 17:34:09 -0500

    añadiendo avances en clase

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+$(document).ready(function() {
+/* Seleccionando por id */
+// console.log($('#p-test').text('Agregando contenido'));
+console.log($('#p-test').html('Hola'));
+console.log($('#input-test').val());
+});
+
+/* Seleccionando por elemento */
+/*
+console.log($('p').text());
+console.log($('p').html());
+console.log($('input').val());*/
+
+/* Seleccionando por clase */
+
+/*
+console.log($('.p-text').text());
+console.log($('.p-text').html());
+console.log($('.input').val());
+*/
 
